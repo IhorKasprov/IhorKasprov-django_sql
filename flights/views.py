e85de06a95bc7aea6afb10fb4cc0784e7882643e
IhorKasprov <kasprovigor@gmail.com>
2023-01-02 22:02:19 +0200

    добавив add passenger

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Flight
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Flight, Passenger
 
 # Створюємо наші views.
 
@@ -13,5 +16,26 @@ def flight(request, flight_id):
     passengers = flight.passengers.all()
     return render (request, "flights/flight.html", {
         "flight": flight,
-        "passengers": passengers
+        "passengers": passengers,
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+def book(request, flight_id):
+
+    # Якщо запит post - додаємо новий рейс
+    if request.method == "POST":
+
+        # Отримуємо доступ до рейсів
+        flight = Flight.objects.get(pk=flight_id)
+
+        # Знаходимо id пасажира через дані з надісланої форми
+        passenger_id = int(request.POST["passenger"])
+
+        # Знаходимо пасажира за id
+        passenger = Passenger.objects.get(pk=passenger_id)
+
+        # Додаємо пасажира на рейс
+        passenger.flights.add(flight)
+
+        # Перенаправляємо користувача на сторінку рейсів
+        return HttpResponseRedirect(reverse("flight", args=(flight.id,)))
